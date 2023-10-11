ade697c3d3d4daaf63294f6f8bdd888003067aff
Fachri Botz <94295494+Ipulpachri@users.noreply.github.com>
2023-10-11 15:30:08 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
-global.owner = ['6281395861695']  
-global.mods = ['6281395861695'] 
+global.owner = ['6285713041886']  
+global.mods = ['6281322667077'] 
 global.prems = ['6281395861695']
-global.nameowner = 'Tio'
-global.numberowner = '6281395861695' 
-global.mail = 'support@tioprm.my.id' 
-global.gc = 'https://chat.whatsapp.com/Ln2vHjRrRayAbzalRMB56r'
-global.instagram = 'https://instagram.com/prm2.0'
-global.wm = '© Tio'
+global.nameowner = 'Fachri'
+global.numberowner = '6285713041886' 
+global.mail = 'fachristyle.id@gmail.com' 
+global.gc = 'https://chat.whatsapp.com/BUU91r7ZQaE1Pw7W78KpSw'
+global.instagram = 'https://instagram.com/sfdesign_id'
+global.wm = '© FachriStyle'
 global.wait = '_*Tunggu sedang di proses...*_'
 global.eror = '_*Server Error*_'
 global.stiker_wait = '*⫹⫺ Stiker sedang dibuat...*'
@@ -14,11 +14,11 @@ global.packname = 'Made With'
 global.author = 'Bot WhatsApp'
 
 //INI WAJIB DI ISI!//
-global.btc = 'YOUR_APIKEY_HERE' 
+global.btc = '477M6Q8M' 
 //Daftar terlebih dahulu https://api.botcahx.live
 
 //INI OPTIONAL BOLEH DI ISI BOLEH JUGA ENGGA//
-global.lann = 'YOUR_APIKEY_HERE'
+global.lann = 'kwfbrmAN'
 //Daftar https://api.betabotz.org 
 
 global.APIs = {   
